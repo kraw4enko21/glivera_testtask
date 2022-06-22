7107acb619c76f057065e01bc0984ca89e447473
kraw4enko21 <kraw4enko21@gmail.com>
2022-06-22 15:22:22 +0300

    First commit

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const btn = document.querySelector('.header__close')
-const block = document.querySelector('.header__block')
-const blockBtn = document.querySelector('.header__authorization')
-const menu = document.querySelector('.modalone__content')
-const menuBtn = document.querySelector('.header__burger')
-const menuLink = document.querySelectorAll('.modalone__item')
-btn.addEventListener('click',()=>{
-    block.classList.add('close')
-    blockBtn.classList.add('getup')
-    menu.classList.add('getup-two')
-})
+// const btn = document.querySelector('.header__close')
+// const block = document.querySelector('.header__block')
+// const blockBtn = document.querySelector('.header__authorization')
+const menu = document.querySelector('.menu__list')
+const menuBtn = document.querySelector('.menu__burger')
+const menuLink = document.querySelectorAll('.menu__link-lists')
+// btn.addEventListener('click',()=>{
+//     block.classList.add('close')
+//     blockBtn.classList.add('getup')
+//     menu.classList.add('getup-two')
+// })
 menuBtn.addEventListener('click',()=>{
     menu.classList.toggle('close')
 })
-console.log(menuLink)
 menuLink.forEach((i)=>{
     i.addEventListener('click',()=>{
-        console.log('click')
         menu.classList.add('close')
     })
 })
